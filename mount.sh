89ae59cf7c723d3082593bdcf9b91b428577b5c5
maxep <me@maxep.me>
2021-01-18 16:06:56 +0100

    Update variables names

--- mount.sh
+++ mount.sh
@@ -4,51 +4,51 @@ set -e
 [ "${DEBUG:-false}" == 'true' ] && { set -x; S3FS_DEBUG='-d -d'; }
 
 # Defaults
-: ${AWS_S3_AUTHFILE:='/root/.s3fs'}
-: ${AWS_S3_MOUNTPOINT:='/srv/airtime/stor'}
-: ${AWS_S3_URL:='https://s3.amazonaws.com'}
+: ${S3_CREDENTIALS_FILE:='/root/.s3fs'}
+: ${S3_MOUNTPOINT:='/srv/airtime/stor'}
+: ${S3_URL:='https://s3.amazonaws.com'}
 : ${S3FS_ARGS:=''}
 
 # If no command specified, print error
 [ "$1" == "" ] && set -- "$@" bash -c 'echo "Error: Please specify a command to run."; exit 128'
 
 # Configuration checks
-if [ -z "$AWS_STORAGE_BUCKET_NAME" ]; then
-    echo "Error: AWS_STORAGE_BUCKET_NAME is not specified"
+if [ -z "$S3_BUCKET" ]; then
+    echo "Error: S3_BUCKET is not specified"
     exit 128
 fi
 
-if [ ! -f "${AWS_S3_AUTHFILE}" ] && [ -z "$AWS_ACCESS_KEY_ID" ]; then
-    echo "Error: AWS_ACCESS_KEY_ID not specified, or ${AWS_S3_AUTHFILE} not provided"
+if [ ! -f "${S3_CREDENTIALS_FILE}" ] && [ -z "$S3_ACCESS_KEY_ID" ]; then
+    echo "Error: S3_ACCESS_KEY_ID not specified, or ${S3_CREDENTIALS_FILE} not provided"
     exit 128
 fi
 
-if [ ! -f "${AWS_S3_AUTHFILE}" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-    echo "Error: AWS_SECRET_ACCESS_KEY not specified, or ${AWS_S3_AUTHFILE} not provided"
+if [ ! -f "${S3_CREDENTIALS_FILE}" ] && [ -z "$S3_SECRET_ACCESS_KEY" ]; then
+    echo "Error: S3_SECRET_ACCESS_KEY not specified, or ${S3_CREDENTIALS_FILE} not provided"
     exit 128
 fi
 
 # Write auth file if it does not exist
-if [ ! -f "${AWS_S3_AUTHFILE}" ]; then
-   echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > ${AWS_S3_AUTHFILE}
-   chmod 400 ${AWS_S3_AUTHFILE}
+if [ ! -f "${S3_CREDENTIALS_FILE}" ]; then
+   echo "${S3_ACCESS_KEY_ID}:${S3_SECRET_ACCESS_KEY}" > ${S3_CREDENTIALS_FILE}
+   chmod 400 ${S3_CREDENTIALS_FILE}
 fi
 
-echo "Mount S3 bucket to ${AWS_S3_MOUNTPOINT}"
-mkdir -p ${AWS_S3_MOUNTPOINT}
+echo "Mount S3 bucket to ${S3_MOUNTPOINT}"
+mkdir -p ${S3_MOUNTPOINT}
 
 # s3fs mount command
 s3fs \
   $S3FS_DEBUG \
-  -o passwd_file=${AWS_S3_AUTHFILE} \
-  -o url=${AWS_S3_URL} \
+  -o passwd_file=${S3_CREDENTIALS_FILE} \
+  -o url=${S3_URL} \
   -o umask=113 \
   -o mp_umask=113 \
   -o uid=`id -u 'www-data'` \
   -o gid=`id -g 'www-data'` \
   -o allow_other \
   $S3FS_ARGS \
-  ${AWS_STORAGE_BUCKET_NAME} \
-  ${AWS_S3_MOUNTPOINT}
+  ${S3_BUCKET} \
+  ${S3_MOUNTPOINT}
 
 exec "$@"
